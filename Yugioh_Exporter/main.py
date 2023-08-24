0ec79b9487fbce604b0fd9a88be63657275e8c62
Jace <54625750+Jacekun@users.noreply.github.com>
2023-08-24 09:14:17 +0800

    Export card listings for non-common cards with price

--- Yugioh_Exporter/main.py
+++ Yugioh_Exporter/main.py
@@ -54,7 +54,7 @@ def write_file(filename: str, content: str) -> bool:
 def write_json(filename: str, content: any) -> bool:
     try:
         with open(filename, 'w') as f:
-            json.dump(content, f)
+            json.dump(content, f, indent = 4)
         return True
     except Exception as e:
         log_err("Error", e)
@@ -70,48 +70,71 @@ def read_json(filename: str) -> any:
     except Exception as e:
         log_err("Error", e)
         return None
+
 # Main
 try:
     # Constants
     folder_setcodes = "setcodes"
     route_setcode = "https://db.ygoprodeck.com/api/v7/cardsetsinfo.php?setcode={0}&includeAliased&num=1&offset=0"
-    index_cardname: int = 3
-    index_cardnumber: int = 6
-    index_qty: int = 1
+    route_image = "https://images.ygoprodeck.com/images/cards/{0}.jpg" # card passcode
     index_folder_name: int = 0
+    index_qty: int = 1
     index_tradeqty: int = 2
+    index_cardname: int = 3
+    index_set_name: int = 5
+    index_cardnumber: int = 6
+    index_rarity: int = 7
+    index_printing: int = 9
+    index_price_low: int = 13
+    index_price_mid: int = 14
+    index_price_market: int = 15
     csv_text_encoding: str = "utf-8"
+    price_conversion_php: float = 55.00
+    # Use for array holders
+    format_tcg: int = 0
+    format_ocg: int = 1
 
     # File paths
     csv_file_name: str = "all-folders-output.csv"
     csv_file_name_source: str = "all-folders"
-    export_json_file_name: str = "cards.json"
-    export_conf_file: str = "MyCards.lflist.conf"
+    # Export filepaths
+    export_json_file_name: list[str] = ["cards_tcg.json", "cards_ocg.json"]
+    export_conf_file: list[str] = ["MyCards_TCG.lflist.conf", "MyCards_OCG.lflist.conf"]
     jsonfile_cards_with_error: str = "cards_with_error.json"
     jsonfile_card_conf_list: str = "conf_cards.json"
+    jsonfile_listings: str = "listings.json"
 
     # Variables
     card_count: int = 0
     sep: str = ","
     conf_contents: str = "#[My Cards]\n!My Cards\n$whitelist\n"
-    skip_folders: list[str] = ['OCG', 'Rush']
+    folder_skip: list[str] = ['Rush']
+    folder_listing: list[str] = ['Binder', 'Gold Binder']
 
     cards: list[any] = [] # List of all cards from Imported csv file
+    cards_ocg: list[any] = [] # OCG List
     cards_with_error: list[any] = [] # List of all cards with error
     card_conf_list: list[any] = [] # List of all card to be put to conf file.
+    card_listings: list[any] = [] # List of card listings with rarity. For shop use.
 
     # Dynamic variables for local use
     jsonfile_setcode: str = ""
     card_setcode: str = ""
+    card_setname: str = ""
+    card_rarity: str = ""
+    card_printing: str = ""
+    card_format: str = "TCG"
     row_card_name: str = ""
     card_folder_name: str = ""
     contents_decode: str = ""
+    card_qty: int = 0
     card_setcode_split: any = None
     card_json: any = None
     card_json_setcode: any = None
     req_object: any = None
     temp_object: any = None
     is_request: bool = False
+    is_ocg: bool = False
 
     # Create necessary folders
     Path(folder_setcodes).mkdir(parents=True, exist_ok=True)
@@ -159,7 +182,7 @@ try:
             if len(first_line_split) > 1:
                 first_line_right = str(first_line_split[1])
                 sep = first_line_right.replace("\"", "").strip()
-                log(f"Separator => {sep}.")
+                log(f"Separator => {sep}")
 
                 remove_first_row = True
 
@@ -191,33 +214,70 @@ try:
                 else:
                     card_folder_name = str(row[index_folder_name])
                     row_card_name = str(row[index_cardname])
-                    if card_folder_name not in skip_folders:
-                        log(f"\tL{line_count}; Processing {row_card_name} with cardset '{row[index_cardnumber]}'")
-                        card_count += 1
-                        # Initialize new card object
-                        card_setcode = str(row[index_cardnumber])
-                        card_setcode_split = card_setcode.split('-')
-                        new_card_object = {
-                            "name": row_card_name,
-                            "set": card_setcode,
-                            "set_global": f"{card_setcode_split[0]}-{str(card_setcode_split[1])[2:]}",
-                            "qty": int(row[index_qty]),
-                            "trade_qty": int(row[index_tradeqty])
-                        }
-                        cards.append(new_card_object)
-                    else:
-                        log(f"Skip card from folder => {card_folder_name}; {row_card_name}")
+                    is_ocg = card_folder_name == "OCG"
+                    card_format = "OCG" if is_ocg else "TCG"
+                    
+                    log(f"\tL{line_count}; Processing {row_card_name} with cardset '{row[index_cardnumber]}'")
+                    card_count += 1
+                    # Initialize new card object
+                    card_setcode = str(row[index_cardnumber])
+                    card_setname = str(row[index_set_name])
+                    card_setcode_split = card_setcode.split('-')
+                    card_rarity = str(row[index_rarity])
+                    card_printing = str(row[index_printing])
+                    card_qty = int(row[index_qty])
+                    new_card_object = {
+                        "name": row_card_name,
+                        "set": card_setcode,
+                        "set_global": f"{ card_setcode_split[0] }-{ str(card_setcode_split[1])[2:] }",
+                        "qty": card_qty,
+                        "trade_qty": int(row[index_tradeqty]),
+                        "format": card_format
+                    }
+                    if card_folder_name not in folder_skip:
+                        if is_ocg:
+                            cards_ocg.append(new_card_object)
+                        else:
+                            cards.append(new_card_object)
+
+                    # Add to listings
+                    #if card_folder_name in folder_listing: # Check condition for listing
+                    if card_rarity != "Common": # Check condition for listing
+                        price_low: float = round(float(row[index_price_low]) * price_conversion_php, 2) if row[index_price_low] else 0
+                        price_mid: float = round(float(row[index_price_mid]) * price_conversion_php, 2) if row[index_price_mid] else 0
+                        price_market: float = round(float(row[index_price_market]) * price_conversion_php, 2) if row[index_price_market] else 0
+                        new_obj_listing = {
+                        "name": f"Yu-Gi-Oh! { card_format } { row_card_name } ({ card_setcode } { card_rarity })",
+                        "qty": card_qty,
+                        "desc": f"""Yugioh { card_format } card
+                            Name: { row_card_name }
+                            Set: { card_setname }
+                            Edition: { card_printing }
+                            Condition: Near Mint
+
+                            (Price based on yugiohprices.com)""".replace('                        ', ''), # looks ugly but necessary to prevent useless whitespaces.
+                        
+                        "price_low": price_low,
+                        "price_mid": price_mid,
+                        "price_market": index_price_market,
+                        "rarity" : card_rarity
+                        } 
+                        card_listings.append(new_obj_listing)
+
     except Exception as e:
         log_err("CSV file error", e)
         raise Exception("CSV File error")
         
     log(f"Processed {card_count} cards.")
 
-    write_json(export_json_file_name, cards)
-    log(f"Exported to file => {export_json_file_name}")
+    write_json(export_json_file_name[0], cards)
+    log(f"Exported TCG card list.")
+
+    write_json(jsonfile_listings, card_listings)
+    log(f"Exported to listing file => {jsonfile_listings}")
 
     # Read from JSON file and request card passcode
-    card_json = read_json(export_json_file_name)
+    card_json = read_json(export_json_file_name[0])
 
     if card_json is not None:
         log("JSON file loaded!")
@@ -332,7 +392,7 @@ try:
     write_json(jsonfile_cards_with_error, cards_with_error)
 
     # Dump file
-    write_file(export_conf_file, conf_contents)
+    write_file(export_conf_file[0], conf_contents)
 
 except Exception as e:
     log_err("Error, main", e)
